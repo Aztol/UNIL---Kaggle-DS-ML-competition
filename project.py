07c026b4882ece9931652eecfff69b59ab9b9030
Aztol <laurent0499@gmail.com>
2023-11-15 11:46:56 +0100

    Updated

--- project.py
+++ project.py
@@ -2,6 +2,16 @@
 def skrrr (nombre):
     return 2*nombre
 
+def tamer (nom):
+    if nom == "Yoann":
+        print("tamerlatepu69")
+    else:
+        print("c tou bon")
+
+
 print(skrrr(10))
 for i in range(9):
     print(skrrr(i))
+
+tamer("Yoann")
+
