a05b32081461249a423da88f5e478249ebdd8af5
Aztol <laurent0499@gmail.com>
2023-11-20 14:18:52 +0100

    done

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,13 +1,16 @@
 import re
 import pandas as pd
 import re
+import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.stem import SnowballStemmer
-import nltk
+
 import string
 
-#
+
+
+
 
 #french_stopwords = set(stopwords.words('french'))
 
--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 from data_cleaning import *
 import nltk
 
+nltk.download('stopwords')
+nltk.download('punkt')
+
 df = pd.read_csv("training_data.csv")
 levels = pd.read_csv("vocabulaires.csv", sep=';')
 
 
-nltk.download('stopwords')
-nltk.download('punkt')
+print(nltk.data.path)
 
 print(df.head())
 print(levels.head())
@@ -15,7 +17,7 @@ print(df.isnull().sum())
 
 #df['clean_text'] = df['sentence'].apply(preprocess_text)
 df['stemmer_sentence'] = df['sentence'].apply(stem_sentences)
-df['levels_score'] = df.apply(calculate_levels_score(df, levels), axis=1)
+#df['levels_score'] = df.apply(calculate_levels_score(df, levels), axis=1)
 
 add_length_column(df)
 
