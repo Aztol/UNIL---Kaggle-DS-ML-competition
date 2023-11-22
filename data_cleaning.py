4a768b173f38c95ca0b3ee028b2ba442c1a605b1
Gaetan <Gaetan1209@gmail.com>
2023-11-22 18:12:00 +0100

    ça été la répet?

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,60 +1,62 @@
-import re
 import pandas as pd
 import re
 import nltk
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize, sent_tokenize
-from nltk.stem import SnowballStemmer
-
+from nltk.tokenize import word_tokenize
 import string
 
+# Téléchargement des ressources NLTK, si nécesf
+#saire
+nltk.download('punkt')
+nltk.download('stopwords')
 
-
-
-
-#french_stopwords = set(stopwords.words('french'))
+# Chargement des stopwords français
+french_stopwords = set(stopwords.words('french'))
 
 def preprocess_text(text):
-    
+    """Nettoie et tokenise le texte."""
+    # Conversion en minuscules
     text = text.lower()
+    
+    # Suppression des caractères non-alphabétiques
     text = re.sub(r'[^a-zA-Z\s]', '', text)
-
+    
+    # Tokenisation
     tokens = word_tokenize(text, language="french")
-    tokens = [mot for mot in tokens if mot.lower() not in stopwords.words('french') and mot not in string.punctuation]
+    
+    # Suppression des stopwords et de la ponctuation
+    tokens = [mot for mot in tokens if mot not in french_stopwords and mot not in string.punctuation]
+    
+    # Retourne les tokens nettoyés sous forme de chaîne de caractères
+    return ' '.join(tokens)
+
+# Charger les données depuis le fichier CSV
+df = pd.read_csv('training_data.csv')
+
+# Appliquer le nettoyage à chaque phrase dans la colonne 'sentence'
+df['cleaned_text'] = df['sentence'].apply(preprocess_text)
+
+# Sauvegarder les données nettoyées dans un nouveau fichier
+df.to_csv('training_data_cleaned.csv', index=False)
 
 
-    #stemmer = SnowballStemmer('french')
-    #text = [stemmer.stem(mot) for mot in tokens]
-    return text
-
-def add_length_column(dataframe):
-
-    dataframe['length'] = dataframe['sentence'].apply(lambda x: len(x))
-
-def stem_sentences(sentence):
-    # Tokenize the sentence and apply stemming using Porter Stemmer
-    stemmer = SnowballStemmer('french')
-    words = word_tokenize(sentence)
-    stemmed_words = [stemmer.stem(word) for word in words]
-    return ' '.join(stemmed_words)
-
-def calculate_levels_score(df, levels):
+#def calculate_levels_score(df, levels):       
     # Check corresponding words and calculate levels_score
     
-    a1a2_words = levels['a1a2'].tolist()
-    b1b2_words = levels['b1b2'].tolist()
-    c1c2_words = levels['c1c2'].tolist()
+    #a1a2_words = levels['a1a2'].tolist()
+    #b1b2_words = levels['b1b2'].tolist()
+    #c1c2_words = levels['c1c2'].tolist()
 
-    levels_score = 0
-    for index, row in df.iterrows():
-        levels_score = 0
-        current_sentence = row['sentence']
-        word = word_tokenize(current_sentence)
-        if word in a1a2_words:
-            levels_score += 1
-        elif word in b1b2_words:
-            levels_score += 2
-        elif word in c1c2_words:
-            levels_score += 3
-        df.at[index, 'levels_score'] = levels_score
-    return df
+    #levels_score = 0
+    #for index, row in df.iterrows():
+        #levels_score = 0
+        #current_sentence = row['sentence']
+        #word = word_tokenize(current_sentence)
+        #if word in a1a2_words:
+            #levels_score += 1
+        #elif word in b1b2_words:
+            #levels_score += 2
+        #elif word in c1c2_words:
+            #levels_score += 3
+        #df.at[index, 'levels_score'] = levels_score
+    #return df
